58b75bf936f7fba9c9a778c9cb7f0e5b7229333d
peterzhu <z18819828123@gmail.com>
2024-04-25 13:50:18 -0500

    update on the mointor

--- project_main.py
+++ project_main.py
@@ -2,6 +2,10 @@ from utils.light_sensor import LightSensor
 from utils.plant_monitor import PlantMonitor
 #from utils.camera_module import capture_image
 import time
+import logging
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+import json
+import time
 import os
 import torch
 import clip
@@ -9,6 +13,20 @@ from PIL import Image
 import json
 from torchvision.datasets import Flowers102
 import os.path as osp
+
+ca = 'ver_key/AmazonRootCA1.pem'
+private_key = 'ver_key/6228bf39f1582a64a893649a1d0f77eb4de1d7574afcdf38a3953c51437d7aa2-private.pem.key'
+certifcate = '/Users/peterzhu/Desktop/598_EverGreen-1/ver_key/6228bf39f1582a64a893649a1d0f77eb4de1d7574afcdf38a3953c51437d7aa2-certificate.pem.crt'
+# For certificate based connection
+myMQTTClient = AWSIoTMQTTClient("test_pi")
+# Update with your AWS IoT endpoint
+myMQTTClient.configureEndpoint("a31v04gy74znbf-ats.iot.us-east-2.amazonaws.com", 8883)
+myMQTTClient.configureCredentials(ca, private_key,certifcate)
+
+# MQTT client connection setup
+myMQTTClient.connect()
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 datadir = "/home/pi/Desktop/598_EverGreen"
 
@@ -42,8 +60,7 @@ def classify_image(image_path, clip_model, clip_preprocess, device, flower_class
     return most_likely_flower, probability
 
 def main():
-    
-    
+    '''
     device = "cuda" if torch.cuda.is_available() else 'cpu'
     clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
     image_path = "/home/pi/Desktop/598_EverGreen/rose.png"
@@ -56,34 +73,31 @@ def main():
     #image_path = capture_image()
     #print('captured image path: ',image_path)
     #most_likely_flower = 'rose'
-    
-    
-    light_sensor = LightSensor()
-    plant_monitor = PlantMonitor()
 
-
-
-    id = 1
+    '''
     while True: 
-        print(f'Time: {id} Sec')
-        
-        # Read and print light intensity
-        light_level = light_sensor.read_light()
-        print(f"Light Intensity: {light_level:.2f} Lux")
 
-        # Read and print plant conditions
-        wetness = plant_monitor.get_wetness()
-        temp = plant_monitor.get_temp()
-        humidity = plant_monitor.get_humidity()
-        print(f"Plant Wetness: {wetness}")
-        print(f"Plant Temperature: {temp}°C")
-        print(f"Plant Humidity: {humidity}%")
-
-        time.sleep(300)  
-        id += 300
-        
-
+        light_sensor = LightSensor()
+        plant_monitor = PlantMonitor()
+        payload = {
+            'timestamp': int(time.time()),
+            'temperature': plant_monitor.get_temp(),
+            'humidity': plant_monitor.get_humidity(),     # Simulated humidity
+            'light_level': light_sensor.read_light()  # Simulated light level
+        }
+        try:
+            myMQTTClient.publish("topic/plant_health", json.dumps(payload), 0)
+            logging.info("Message published")
+        except Exception as e:
+            logging.error(f"Failed to publish message: {e}")
+        time.sleep(10)
 
 
 if __name__ == '__main__':
+    try:
+        myMQTTClient.connect()
+        logging.info("Connected to AWS IoT")
+    except Exception as e:
+        logging.error(f"Failed to connect to AWS IoT: {e}")
+        exit(1)
     main()
