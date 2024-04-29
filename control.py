132c2aba75c92a9813750272da11ccf047eb2b9f
peterzhu <z18819828123@gmail.com>
2024-04-29 11:21:58 -0500

    stop the cv for now

--- control.py
+++ control.py
@@ -7,10 +7,10 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 import json
 from datetime import datetime
 import lib8relind
-import torch
-import torchvision.models as models
-import torchvision.transforms as transforms
-from PIL import Image
+#import torch
+#import torchvision.models as models
+#import torchvision.transforms as transforms
+#from PIL import Image
 import os.path as osp
 
 # Paths to the certificate files
@@ -21,7 +21,7 @@ stack_level = 0  # Adjust based on your jumper settings
 relay_number = 5  # Relay connected to the water pump
 datadir = "/home/pi/Desktop/598_EverGreen"
 image_path = osp.join(datadir, "rose.png")
-
+'''
 # Define expanded flower classes
 flower_classes = ['tulip', 'daisy', 'sunflower','daffodil','rose']
 
@@ -37,7 +37,7 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalization for MobileNet
 ])
 
-
+'''
 # Create and configure the MQTT client
 myMQTTClient = AWSIoTMQTTClient("test_pi")
 myMQTTClient.configureEndpoint("a31v04gy74znbf-ats.iot.us-east-2.amazonaws.com", 8883)
@@ -77,8 +77,8 @@ def customCallback(client, userdata, message):
 
 def main():
 
-    predicted_class = classify_image(image_path, model, transform)
-    print(f'Predicted Flower Type: {predicted_class}')
+    #predicted_class = classify_image(image_path, model, transform)
+    #print(f'Predicted Flower Type: {predicted_class}')
     
     myMQTTClient.subscribe("topic/command", 1, customCallback)
     light_sensor = LightSensor()
